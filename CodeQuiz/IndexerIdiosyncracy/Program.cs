a8146e1b66ae13321df113d5def962d11abe42cf
PHOENIX\kims1 <kims1@apotek1.net>
2023-10-14 09:06:33 +0200

    Added IndexerIdiosyncracy

---
+++ CodeQuiz/IndexerIdiosyncracy/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Specialized;
+using static System.Console;
+
+try
+{
+    var empty = new NameValueCollection()["green"];
+    WriteLine("green!");
+
+    var alsoEmpty = new Dictionary<string, string>()["blue"];
+    WriteLine("blue!");
+}
+catch (Exception ex)
+{
+    WriteLine(ex.GetType().Name);
+}
+/*
+    green!
+    KeyNotFoundException
+*/
